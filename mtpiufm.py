666c8dfb3494b37e00d92ea5e900e726416bfc7c
Andre Miras <andre.miras@gmail.com>
2014-04-16 00:30:28 +0200

    initial commit

---
+++ mtpiufm.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python2
+
+import os
+import tempfile
+import mechanize
+import webbrowser
+from cred import credential
+
+# https://ent.montpellier.iufm.fr/cas/index.jsp?service=http://ent.montpellier.iufm.fr/Login
+# https://ent.montpellier.iufm.fr/cas/
+# http://ent.montpellier.iufm.fr/Login
+# https://ent.montpellier.iufm.fr/cas/?service=http://web2.montpellier.iufm.fr/WD110AWP/WD110Awp.exe/CONNECT/Planning
+# http://web2.montpellier.iufm.fr/WD110AWP/WD110Awp.exe/CONNECT/Planning
+
+
+
+class MtpIufmBrowser:
+
+    def __init__(self):
+        self.LOGIN_URL = "https://ent.montpellier.iufm.fr/cas/"
+        self.PLANNING_URL = "http://web2.montpellier.iufm.fr/WD110AWP/WD110Awp.exe/CONNECT/Planning"
+        self.browser = mechanize.Browser()
+        # ignores HTTP Error 403: request disallowed by robots.txt
+        self.browser.set_handle_robots(False)
+
+    def login(self, username, password):
+        """
+        Returns True on login success.
+        """
+        self.username = username
+        self.password = password
+        browser = self.browser
+        browser.open(self.LOGIN_URL)
+        browser.select_form(name="login_form")
+        # browser.form['username'] = credential["username"]
+        # browser.form['password'] = credential["password"]
+        browser.form['username'] = username
+        browser.form['password'] = password
+        resp = browser.submit()
+        return "mot de passe invalide" not in resp.read()
+
+    def planning_html(self):
+        """
+        Returns planning HTML code.
+        """
+        browser = self.browser
+        # browses to planning form
+        # TODO: verify we don't do things twice
+        browser.open(self.PLANNING_URL)
+        browser.follow_link(url_regex='/CONNECT/Planning')
+        # posts planning form
+        browser.select_form(name="MENU")
+        # from date -> default
+        browser.form["A1"] 
+        # to date -> default
+        browser.form["A6"] 
+        # format -> HTML
+        HTML_OPTION = ["2"]
+        browser.form["A4"] = HTML_OPTION
+        resp = browser.submit()
+        return resp.read()
+
+
+def main():
+    mtpIufmBrowser = MtpIufmBrowser()
+    mtpIufmBrowser.login(
+        credential["username"],
+        credential["password"])
+    html = mtpIufmBrowser.planning_html()
+    f = tempfile.NamedTemporaryFile(delete=False)
+    f.write(html)
+    f.close()
+    new = 2 # open in a new tab, if possible
+    webbrowser.open(f.name, new=new)
+    # os.unlink(f.name)
+
+if __name__ == "__main__":
+    main()
---
+++ site.py
@@ -0,0 +1,43 @@
+from flask import Flask, request, render_template, flash, redirect, url_for
+import settings
+from mtpiufm import MtpIufmBrowser
+app = Flask(__name__)
+app.secret_key = settings.SECRET_KEY
+
+@app.route('/')
+def index():
+    return redirect(url_for('login'))
+
+def valid_login(username, password):
+    mtpIufmBrowser = MtpIufmBrowser()
+    return mtpIufmBrowser.login(username, password)
+
+def show_planning(username, password):
+    mtpIufmBrowser = MtpIufmBrowser()
+    # TODO: do not login twice (the first is in valid_login
+    mtpIufmBrowser.login(username, password)
+    planning_html = mtpIufmBrowser.planning_html()
+    # return render_template('planning.html', planning_html=planning_html)
+    return planning_html
+
+@app.route('/login/', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        if valid_login(request.form['username'],
+                       request.form['password']):
+            # return log_the_user_in(request.form['username'])
+            return show_planning(request.form['username'],
+                        request.form['password'])
+        else:
+            flash(u'Invalid username/password', 'danger')
+    # the code below is executed if the request method
+    # was GET or the credentials were invalid
+    return render_template('login.html')
+
+@app.route('/hello')
+def hello():
+    return 'Hello World!'
+
+if __name__ == '__main__':
+    app.debug = settings.DEBUG
+    app.run(port=8000)
